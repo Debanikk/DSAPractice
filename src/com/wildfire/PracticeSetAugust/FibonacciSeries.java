ef3ab26ae13ffbc9a6e842593d3dfe7bbbb1b052
Debanikk <debanikkundu2014@gmail.com>
2023-10-11 20:31:29 -0400

    DSA Practice codes with searching sorting and multithreading practice

--- src/com/wildfire/PracticeSetAugust/FibonacciSeries.java
+++ src/com/wildfire/PracticeSetAugust/FibonacciSeries.java
@@ -16,8 +16,11 @@ public class FibonacciSeries {
         int i;
         fib[0] = 0;
         fib[1] = 1;
+        System.out.println(fib[0]);
+        System.out.println(fib[1]);
         for(i = 2; i < n + 2; i++) {
             fib[i] = fib[i-1] + fib[i-2];
+            System.out.println(fib[i]);
         }
         return fib[n-1];
     }
